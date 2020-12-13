1c54e5cb64b4e01e3f1ffdad8d4eb918720eb824
dd <778418556@qq.com>
2020-12-13 23:08:26 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+// home页面
+<template>
+  <div>
+    <el-button @click="logout" type="info">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 退出按钮
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 引入全局样式
+import './assets/css/global.css'
+// 引入图标字体
+import './assets/fonts/iconfont.css'
+// 全局引入axios
+import axios from 'axios'
+// axios配置
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
